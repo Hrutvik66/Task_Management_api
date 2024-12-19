3dacd50475bfc9565b4b713b5b4e9211fc6f3608
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2024-12-19 22:24:10 +0530

    fix: Build removed from git

--- build/database/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize('postgres://postgres:postgres@localhost:5432/node-api', {
-    logging: false,
-    dialect: 'postgres',
-});
-exports.default = sequelize;
